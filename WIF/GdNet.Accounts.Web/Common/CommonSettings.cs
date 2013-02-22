4c7e206856313178704c7548955970b56382db8b
juanonsoftware <huanhvhd@gmail.com>
2013-02-22 18:05:56 +0200

    Add MVC app

---
+++ WIF/GdNet.Accounts.Web/Common/CommonSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace GdNet.Accounts.Web.Common
+{
+    public class CommonSettings
+    {
+        public const string IssuerName = "IssuerName";
+        public const string SigningCertificateName = "SigningCertificateName";
+        public const string EncryptingCertificateName = "EncryptingCertificateName";
+    }
+}
